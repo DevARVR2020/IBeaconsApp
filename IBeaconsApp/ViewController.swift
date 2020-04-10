dc0ad73c7dc35d80cb3307fa5d6e072662a74cf8
KRISHNAVENI ERAMALLA <KRISHNAVENI.ERAMALLA@ALTICEUSA.COM>
2020-04-10 13:48:33 +0530

    Initial Commit

---
+++ IBeaconsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBeaconsApp
+//
+//  Created by Krishnaveni on 4/10/20.
+//  Copyright © 2020 Krishnaveni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
